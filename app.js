f5406bd77762f6ab1aa6b02ac1953de8e5f2088b
Nieblagrisic <isabella.nx@gmail.com>
2025-03-02 12:29:02 -0300

    actualizo app.js

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let amigo = [];
+let amigo =[];
 
-function agregarAmigos () {
+function agregarAmigo() {
     let inputAmigo = document.getElementById("amigo");
     let nombreAmigo = inputAmigo.value;
 
     //console.log(amigo); me permitio comprobar que el array funcionaba.
     
     if (!nombreAmigo){  //Validación de campo vacío, el ! es para negar la condición.
-        alert("Recuerda ingresar un nombre");
+        alert("Recuerda ingresar un nombre de los participantes");
         return;
     }
     amigo.push(nombreAmigo);
     //console.log(amigo);
-    inputAmigo.value = "";
+    inputAmigo.value ="";
     inputAmigo.focus(); //Para que el cursor se mantenga en el input.
     actualizarAmigos();
 }
- function actualizarAmigos(){
+
+ function actualizarAmigos() {
     let listaAmigos = document.getElementById("listaAmigos");
     listaAmigos.innerHTML = ""; //para limpiar la lista de amigos.
 
@@ -25,10 +26,10 @@ function agregarAmigos () {
         let item = document.createElement("li");
         item.textContent = amigo[i];
         listaAmigos.appendChild(item);//para agregar el item a la lista.
-    }
-    
+    }  
  }
- function sorteoAmigo(){
+
+ function sortearAmigo(){
     if (amigo.length ===0){ //= atribución, ==comparación valores, ===comparación valores y tipo de dato. 
         alert("No hay amigos para sortear");
         return;
@@ -40,3 +41,24 @@ function agregarAmigos () {
     let limpiarLista = document.getElementById("listaAmigos");
     limpiarLista.innerHTML = ""; 
  }
+
+function limpiarTodo() { 
+    /*document.querySelector('#amigo').value = "";
+    document.querySelector('#resultado').innerHTML = "";
+    document.querySelector('#listaAmigos').innerHTML = "";*/
+    amigo =[];
+    let listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = "";
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML ="";
+
+}
+
+function resetearJuego() {
+    limpiarTodo();
+    let inputAmigo = document.getElementById("amigo");
+    inputAmigo.focus();
+    /*let limpiarInput = document.getElementById("resultado");
+    limpiarInput.innerHTML = "";*/
+    /*document.getElementById('#reiniciarJuego').setAttribute('disabled', 'true');*/
+}
